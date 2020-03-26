93016aa700a7537c2e9f9e334a781112fa8cd915
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2020-03-26 19:05:44 -0400

    GitHub social

--- js/components/Footer.jsx
+++ js/components/Footer.jsx
@@ -8,11 +8,22 @@ class Footer extends React.Component {
 			<footer>
 				<div class="left">Made with 🤍 and React</div>
 				<div class="right">
-					<a href="https://discord.gg/c9ESSur" target="_blank"><img
-						class="clickable"
-						src="./images/discord.svg"
-						alt="Discord"
-					/></a>
+					<a
+						id="github"
+						class="social"
+						href="https://github.com/KyzaGitHub/SoulFinder"
+						target="_blank"
+					>
+						<img src="./images/github.png" alt="GitHub" />
+					</a>
+					<a
+						id="discord"
+						class="social"
+						href="https://discord.gg/c9ESSur"
+						target="_blank"
+					>
+						<img src="./images/discord.svg" alt="Discord" />
+					</a>
 				</div>
 			</footer>
 		);
