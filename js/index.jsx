814aa835d6ac124a69870e91435bbcc83bb9ae68
Kyza <40003440+KyzaGitHub@users.noreply.github.com>
2020-03-26 20:51:38 -0400

    Finished The Barn

--- js/index.jsx
+++ js/index.jsx
@@ -22,11 +22,71 @@ let data = {
 				z: 0
 			},
 			video: "e2kHdywdJTY",
-			description:
-				"Behind the flames in the fireplace."
-		},
+			description: "Behind the flames in the fireplace."
+		}
 	},
-	"The Barn": {},
+	"The Barn": {
+		"East Aqueduct": {
+			coords: {
+				x: 182,
+				y: 99,
+				z: -235
+			},
+			description: "In the East Aqueduct."
+		},
+		"Windmill 1": {
+			coords: {
+				x: 96,
+				y: 96,
+				z: -287
+			},
+			items: ["Ender Pearl"],
+			description: "On the 3rd floor of the Windmill."
+		},
+		"Windmill 2": {
+			coords: {
+				x: 99,
+				y: 112,
+				z: -274
+			},
+			items: ["Ender Pearl"],
+			description: "On the sail of the Windmill."
+		},
+		Chimney: {
+			coords: {
+				x: 96,
+				y: 96,
+				z: -287
+			},
+			items: ["Grappling Hook", "Ender Pearl"],
+			description: "Next to the front chimney on top of the barn."
+		},
+		"East Mountains": {
+			coords: {
+				x: 183,
+				y: 99,
+				z: -304
+			},
+			description: "Behind the east mountains."
+		},
+		"West Mountains": {
+			coords: {
+				x: 157,
+				y: 23,
+				z: -202
+			},
+			description: "Inside the water coming from the west mountain."
+		},
+		"The Root": {
+			coords: {
+				x: 157,
+				y: 23,
+				z: -202
+			},
+			description:
+				"At the lowest point of root at the south of the island."
+		}
+	},
 	"Mushroom Desert": {},
 	"Gold Mine": {},
 	"Deep Caverns": {},
